1a53fcdc58a74f1ae58267fda96cfd5b571e21a6
gavinmcfall <gavin@nerdz.co.nz>
2024-04-24 20:00:30 +1200

    learning(concatenation):

--- Week 04/day 03/strings/script.js
+++ Week 04/day 03/strings/script.js
@@ -1,12 +1,12 @@
 console.log("External script file is working!");
 
-const question1 = `"Shall we go?" she said.`;
-const statement1 = `Remember to say "please" 'and' "thank you"`;
-const statement2 = 'Remember to say "please" \'and\' "thank you"';
-const statement3 = "Remember to say " + '"please" ' + "'and' " + '"thank you"';
+const string1 = '"Concentrate all your thoughts ';
+const string2 = "upon the work in hand. The sun's rays do not burn ";
+const string3 = 'until brought to a focus."';
+const string4 = " -- Alexander Graham Bell";
 
-console.log("question1: ", question1, question1.length);
-console.log("statement1: ", statement1, statement1.length);
-console.log("statement2: ", statement2, statement2.length);
-console.log("statement3: ", statement3, statement3.length);
-console.log("question 1 count: ", question1[7]);
+console.log(string1 + string2 + string3 + string4);
+console.log(
+  "Total Length: ",
+  string1.length + string2.length + string3.length + string4.length
+);
