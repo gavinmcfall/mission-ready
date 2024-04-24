051c1ef9852418ec3ec6211145f4147d4b32cbbb
gavinmcfall <gavin@nerdz.co.nz>
2024-04-24 20:16:09 +1200

    learning(JS): Adding and counting

--- Week 04/day 03/strings/script.js
+++ Week 04/day 03/strings/script.js
@@ -1,12 +1,10 @@
 console.log("External script file is working!");
 
-const string1 = '"Concentrate all your thoughts ';
-const string2 = "upon the work in hand. The sun's rays do not burn ";
-const string3 = 'until brought to a focus."';
-const string4 = " -- Alexander Graham Bell";
+const height = 1.83;
+const weight = 100;
+const bookCost = 5;
+const bookCount = 10;
 
-console.log(string1 + string2 + string3 + string4);
-console.log(
-  "Total Length: ",
-  string1.length + string2.length + string3.length + string4.length
-);
+console.log("BMI: " + (weight / (height * height)).toFixed(2));
+const totalCost = bookCost * bookCount;
+console.log("Total cost: $" + totalCost);
