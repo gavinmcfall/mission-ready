c67a8d383b48d8210252ce7aeaef1b98bc228f52
gavinmcfall <gavin@nerdz.co.nz>
2024-04-24 19:02:52 +1200

    learning(data types):

---
+++ Week 04/day 03/strings/script.js
@@ -0,0 +1,16 @@
+console.log("External script file is working!");
+
+let myNumber = 25;
+console.log(typeof myNumber);
+
+let myString = "Hello";
+console.log(typeof myString);
+
+let isRaining = false;
+console.log(typeof isRaining);
+
+let iAmNotDefined;
+console.log(typeof iAmNotDefined);
+
+let iAmAnEmptyValue = null;
+console.log(typeof iAmAnEmptyValue);
--- Week 04/day02/js-intro/script.js
+++ Week 04/day02/js-intro/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World from external file");
+const firstAndLastName = "Gavin McFall";
+let age = 37;
+const ageCalc = 2024 - 1986
+console.log("Name: " + firstAndLastName);
+console.log("Age Actual:" + age);
+console.log("Age Calc: " + ageCalc);
