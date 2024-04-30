9461cc409d6b7cbc25ea09d7b5102f8f3ba9b94a
gavinmcfall <gavin@nerdz.co.nz>
2024-04-30 18:31:02 +1200

    learn(js): Booleans

---
+++ Week 05/day 02/boolean/script.js
@@ -0,0 +1,15 @@
+console.log("Connected!");
+//copy line/s of code and paste it/them below the original line/s of code
+// Shift + Alt + ⬇️
+console.log(Boolean("Hello World!"));
+console.log(Boolean(" ")); //true, space is a value
+console.log(Boolean("")); //false, empty string
+console.log(Boolean(15)); //true, number
+console.log(Boolean(0)); //false, 0 is a falsy value
+console.log(Boolean(-15)); //true, negative number
+
+console.log(Boolean()); //false, undefined
+console.log(Boolean([])); //true, array is an object
+console.log(Boolean(null)); //false, null is a falsy value
+console.log(Boolean(NaN)); //false, NaN is a falsy value
+console.log(Boolean(undefined)); //false, undefined is a falsy value
