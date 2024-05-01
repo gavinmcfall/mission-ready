d32dd145acda16e1ca0549c8ac4b41a7414eb5c4
gavinmcfall <gavin@nerdz.co.nz>
2024-05-01 19:07:31 +1200

    learn(objects): dogs

---
+++ Week 05/day 03/script.js
@@ -0,0 +1,20 @@
+console.log("Connected!");
+
+const dog = {
+  name: "Nova",
+  weight: 8,
+  breed: "Miniature Schnauzer",
+  favActivities: ["walks", "Naping", "Bark & Chase"],
+};
+
+console.log(
+  "This is " +
+    dog.name +
+    " and she loves: " +
+    dog.favActivities[0] +
+    " , " +
+    dog.favActivities[1] +
+    " and " +
+    dog.favActivities[2] +
+    "."
+);
