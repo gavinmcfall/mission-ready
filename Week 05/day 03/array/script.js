d39913f0b1c8e2fe4950e0a6effade5267c2d0bd
gavinmcfall <gavin@nerdz.co.nz>
2024-05-01 20:54:26 +1200

    learn(objects & Arrays): stuff

---
+++ Week 05/day 03/array/script.js
@@ -0,0 +1,12 @@
+console.log("Don't Panic!");
+
+const foods = [
+  "Lousiana Style Pork Ribs",
+  "Brisket",
+  "Beef Shorties",
+  "Pulled Lamb",
+  "Pork Money Muscle",
+];
+console.log(foods);
+
+console.log(foods[0]);
