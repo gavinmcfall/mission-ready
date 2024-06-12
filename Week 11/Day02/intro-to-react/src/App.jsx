6160935f2b20551700f7f09134fb4b02c8c126da
gavinmcfall <gavin@nerdz.co.nz>
2024-06-12 19:46:02 +1200

    chore(week11-day02) - Reach initial stuff

--- Week 11/Day02/intro-to-react/src/App.jsx
+++ Week 11/Day02/intro-to-react/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const message1 = "My old friend";
-  const message2 = "I've come to talk with you again";
+  const message2 = "I've come to";
   return (
     <>
+      <Header />
       <h1>Hello Darkness</h1>
       <h2>{message1}</h2>
-      <p>{message2}</p>
+      <p>
+        {message2 + " talk with you again:"}
+      </p>
+      <p>
+      {((1330 / 2) + (20 * 2) - 40) + " The neighbour of the beast"}
+      </p>
+      <Footer />
     </>
   );
 }
